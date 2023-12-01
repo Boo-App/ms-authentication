e89781a5e963172f99c2859b0570393dca1552a4
Antonin15-76 <antonin.girard.98@gmail.com>
2023-12-01 15:22:52 +0100

    cluster

--- db/clientMongo.ts
+++ db/clientMongo.ts
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
 require('dotenv').config();
 const mongoUri =
-  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@users.pnxzv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@clustertest.i9bfzu2.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
+  console.log('6', mongoUri)
 const uri = encodeURI(String(mongoUri));
 const mongoConnection = mongoose.connect(uri);
 
